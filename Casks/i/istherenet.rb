55122ba365ca7760c79315c1745233b79b16aa65
singularitti <singularitti@outlook.com>
2024-07-07 12:17:51 -0600

    Fix "Verified URL parameter value should end with a /" in `istherenet`

--- Casks/i/istherenet.rb
+++ Casks/i/istherenet.rb
@@ -3,7 +3,7 @@ cask "istherenet" do
   sha256 "34665004bccd872c75f6718399688c87b06e396c22948d4d8576c4e50b84a2d5"
 
   url "https://github.com/FuzzyIdeas/IsThereNet/releases/download/v#{version}/IsThereNet.zip",
-      verified: "github.com/FuzzyIdeas/IsThereNet"
+      verified: "github.com/FuzzyIdeas/IsThereNet/"
   name "IsThereNet"
   desc "Watches for internet connection status changes"
   homepage "https://lowtechguys.com/istherenet/"
